9a0479efd059361089336b63b4b5579ca4b3ec91
Damon German <Damon.German@hyland.com>
2018-09-17 15:04:03 -0500

    Made some changes

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -5,29 +5,37 @@ namespace Delegates
     internal class Program
     {
         public delegate int Transformer(int x);
-        public delegate void Voider(int x);
+        public delegate void Void(int x);
 
         private static void Main(string[] args)
         {
             Transformer t1 = Square;
-            Console.WriteLine(t1(10));
-
             Transformer t2 = delegate(int x) { return x * x; };
             Transformer t3 = x => { return x * x; };
             Transformer t4 = x => x * x;
 
+            int LocalSquare(int x)
+            {
+                return x * x;
+            }
+
+            Transformer t5 = LocalSquare;
+
+            Console.WriteLine(t1(3));
             Console.WriteLine(t2(3));
             Console.WriteLine(t3(3));
             Console.WriteLine(t4(3));
+            Console.WriteLine(t5(3));
+
 
             Func<int, int> func = i => i + 1;
             Console.WriteLine(func(100));
 
-            Voider voiders = Console.WriteLine;
-            voiders += Console.WriteLine;
-            voiders += Console.WriteLine;
+            Void voids = Console.WriteLine;
+            voids += Console.WriteLine;
+            voids += Console.WriteLine;
 
-            voiders(2);
+            voids(2);
         }
 
         public static int Square(int x)
